d845e2fd9afe37c5cff283f52fb50c2fc133b7c8
Javier Hurtado <javier.hurtado0815@gmail.com>
2026-06-14 23:25:58 -0400

    avance gestion de usuarios

---
+++ juegos-salo/src/app/components/registro/registro.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './registro.html',
+  styleUrl: './registro.css',
+})
+export class Registro {
+  registroForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password : ['', [Validators.required,
+                       Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&.=#\-]{6,18}$/),
+      ]],
+      confirmPassword: ['', Validators.required],
+      fechaNacimiento: ['', this.validarEdad],
+      direccion: ['', Validators.required],
+    }, { validators: this.passwordsIguales });
+  }
+
+  validarEdad(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) return null;
+    
+    const fechaNacimiento = new Date(control.value);
+    const hoy = new Date();
+    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    if (hoy < new Date(hoy.getFullYear(), fechaNacimiento.getMonth(), fechaNacimiento.getDate())) edad--;
+    return edad <13 ? {menorEdad: true} : null;
+
+
+  }
+
+  passwordsIguales(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsNoCoinciden: true };
+  }
+
+
+  registrar(){
+    if (this.registroForm.valid) {
+      console.log('Registro exitoso:', this.registroForm.value);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
