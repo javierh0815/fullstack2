5bc65280389a26019e54a5b0b5310cc8c697f01f
Javier Hurtado <javier.hurtado0815@gmail.com>
2026-06-26 19:46:05 -0400

    home ajustado ev2

--- juegos-salo/src/app/components/home/home.ts
+++ juegos-salo/src/app/components/home/home.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { ProductoService } from '../../services/producto-service';
+import { Producto } from '../../models/producto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  private productoService = inject(ProductoService);
+  productosDescuento = signal<Producto[]>(this.productoService.obtenerProductoDescuento());
+
+
+}
